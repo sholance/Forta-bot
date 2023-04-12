1622e53e30c9cf471ac96c60840596a83df53975
Victor Samuel <54833975+sholance@users.noreply.github.com>
2023-04-12 09:22:52 +0000

    contract address method updated

--- src/liquidity.pool.monitor.ts
+++ src/liquidity.pool.monitor.ts
@@ -1,6 +1,7 @@
-import { Finding, HandleTransaction, FindingSeverity, FindingType, TransactionEvent, EntityType, getEthersProvider, LogDescription } from "forta-agent";
+import { Finding, HandleTransaction, FindingSeverity, FindingType, TransactionEvent, EntityType, getEthersProvider, LogDescription, ethers } from "forta-agent";
 import { utils } from "ethers";
 import { PoolFetcher } from "./utils";
+import { TRANSFER_EVENT_ABI } from "./constants";
 
 // This is the address of the token and the events in the liquidity contract we're monitoring
 const { TOKEN_ADDRESS, SWAP_FACTORY_ADDRESSES, PAIRCREATED_EVENT_ABI, POOLCREATED_EVENT_ABI, NEWPOOL_EVENT_ABI, ADDLIQUIDITY_EVENT_ABI } = require("./constants");
@@ -17,59 +18,69 @@ export const provideHandleTransaction = (alertId: string, swapFactoryAddresses: 
     let fetcher = new PoolFetcher(getEthersProvider());
     const block = txEvent.blockNumber;
 
-    // const log: LogDescription[] = txEvent.filterLog(PAIRCREATED_EVENT_ABI);
-
-    // Get all PairCreated and AddLiquidity events for each EVM
     for (const [evmName, swapFactoryAddress] of Object.entries(swapFactoryAddresses)) {
       const pairCreatedEvents = txEvent.filterLog(PAIRCREATED_EVENT_ABI, swapFactoryAddress);
       const poolCreatedEvents = txEvent.filterLog(POOLCREATED_EVENT_ABI, swapFactoryAddress);
       const newPoolEvents = txEvent.filterLog(NEWPOOL_EVENT_ABI, swapFactoryAddress);
-      const addLiquidityEvents = txEvent.filterLog(ADDLIQUIDITY_EVENT_ABI, trackedTokenAddress);
+
       let tokenAddress: string | undefined;
-      if (pairCreatedEvents.length > 0) {
+      if (pairCreatedEvents.length > 0 || poolCreatedEvents.length > 0 || newPoolEvents.length > 0) {
         tokenAddress = pairCreatedEvents[0].args.token0.toLowerCase() || poolCreatedEvents[0].args.token0.toLowerCase() || newPoolEvents[0].args.token0.toLowerCase();
       }
-      let contractAddress: string | undefined;
+      let pairAddress: string | undefined;
       if (pairCreatedEvents.length > 0) {
-        contractAddress = pairCreatedEvents[0].args.sender.toLowerCase() || poolCreatedEvents[0].args.sender.toLowerCase() || newPoolEvents[0].args.sender.toLowerCase();
+        pairAddress = pairCreatedEvents[0].args.pair.toLowerCase();
       }
+      const transferEvents = txEvent.filterLog(TRANSFER_EVENT_ABI, tokenAddress);
       let transaction = txEvent.transaction;
+
+      for (const event of transferEvents) {
+
       // Checks to see if no one else deposits liquidity in the token's the liquidity pool
-      if (addLiquidityEvents.length === 0 && (pairCreatedEvents.length > 0 || poolCreatedEvents.length > 0 || newPoolEvents.length > 0)) {
-        const tokenSymbol = await fetcher.getTokenSymbol(block - 1, tokenAddress!); // Get token symbol using custom function
-        findings.push(
-          Finding.fromObject({
-            name: `No Liquidity Deposits in ${tokenAddress}`,
-            description: `No one has deposited liquidity into the pool for the tracked token on ${evmName}`,
-            alertId: alertId,
-            severity: FindingSeverity.Medium,
-            type: FindingType.Suspicious,
-            labels: [
-              {
-                entityType: EntityType.Address,
-                entity: tokenAddress!,
-                label: "token-address",
-                confidence: 0.8,
-                remove: false,
-              },
-              {
-                entityType: EntityType.Transaction,
-                entity: pairCreatedEvents[0].args.pair.toLowerCase(),
-                label: "pair-monitored",
-                confidence: 0.8,
-                remove: false,
-              }
-            ],
-            metadata: {
-              tokenSymbol: JSON.stringify(tokenSymbol),
-              attackerAddress: JSON.stringify(transaction.from),
-              transaction: JSON.stringify(transaction.hash),
-              tokenAddress: tokenAddress!,
-              contractAddress: JSON.stringify(contractAddress),
-              deployer: JSON.stringify(transaction.from),
-            },
-          })
-        );
+        if (event.args.to === tokenAddress && (pairCreatedEvents.length > 0 || poolCreatedEvents.length > 0 || newPoolEvents.length > 0)) {
+          try {
+            console.log(event.args.to);
+            console.log(transaction.to);
+            const tokenSymbol = await fetcher.getTokenSymbol(block - 1, tokenAddress!); // Get token symbol using custom function
+            findings.push(
+              Finding.fromObject({
+                name: `No Liquidity Deposits in ${tokenAddress}`,
+                description: `No one has deposited liquidity into the pool for the tracked token on ${evmName}`,
+                alertId: alertId,
+                severity: FindingSeverity.Medium,
+                type: FindingType.Suspicious,
+                labels: [
+                  {
+                    entityType: EntityType.Address,
+                    entity: tokenAddress!,
+                    label: "token-address",
+                    confidence: 0.8,
+                    remove: false,
+                  },
+                  {
+                    entityType: EntityType.Transaction,
+                    entity: pairAddress!,
+                    label: "pair-monitored",
+                    confidence: 0.8,
+                    remove: false,
+                  }
+                ],
+                metadata: {
+                  tokenSymbol: JSON.stringify(tokenSymbol),
+                  attackerAddress: JSON.stringify(transaction.from),
+                  transaction: JSON.stringify(transaction.hash),
+                  tokenAddress: tokenAddress!,
+                  contractAddress: JSON.stringify(transaction.to),
+                  deployer: JSON.stringify(transaction.from),
+                },
+              })
+            );
+          }
+          catch (error) {
+            console.log(error);
+          }
+
+        }
       }
     }
 
